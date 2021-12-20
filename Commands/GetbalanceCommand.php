d6157cfce5f1b4e96efea56f29244c83be44d838
MrPark97 <evgeniy.pak.97@mail.ru>
2021-12-20 10:10:06 +0500

    sdffsdsdf

---
+++ Commands/GetbalanceCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace Longman\TelegramBot\Commands\UserCommands;
+use PDO;
+use PDOException;
+
+use Longman\TelegramBot\Commands\UserCommand;
+use Longman\TelegramBot\Request;
+use Longman\TelegramBot\Entities\ServerResponse;
+
+require_once "Models/Balance.php";
+
+// Load all configuration options
+/** @var array $config */
+$config = require __DIR__ . '/../config.php';
+
+use Models\Balance;
+
+class GetbalanceCommand extends UserCommand
+{
+    protected $name = 'getbalance';                      // Your command's name
+    protected $description = 'Get stock leftovers';              // Your command description
+    protected $usage = '/getbalance';                    // Usage of your command
+    protected $version = '1.0.0';                      // Version of your command
+
+    public function execute(): ServerResponse
+    {
+        global $config;   
+        $message = $this->getMessage();            // Get Message object
+
+        $chat_id = $message->getChat()->getId();   // Get the current Chat ID
+        $from       = $message->getFrom();
+        $user_id    = $from->getId();
+        $username = "@".$from->getUsername();
+        $name = $from->getFirstName();
+        
+        $balance = new Balance();
+
+        $data = [                                  // Set up the new message data
+            'chat_id' => $chat_id,                 // Set Chat ID to send the message to
+            'document'=> Request::encodeFile('/Users/evgeniypak/Sites/example/'.$balance->getFilename()),
+        ];
+
+        return Request::sendDocument($data);        // Send message!
+    }
+}
---
+++ Models/Balance.php
@@ -0,0 +1,107 @@
+<?php
+namespace Models;
+use PDO;
+
+// Load all configuration options
+/** @var array $config */
+$config = require_once 'config.php';
+
+class Balance {
+    private $filename = 'balance.csv';
+    private $comments = array();
+
+    public function __construct() {
+        $filename = $this->getFilename();
+        $this->writeData($filename);
+    }
+
+    public function getFilename() {
+        global $config;
+        $cur_dir = $config['data_directory']."/".date("Y");
+        if(!is_dir($cur_dir)) {
+            print($cur_dir);
+            mkdir($cur_dir);
+        }
+        $cur_dir .= "/".date("F");
+        if(!is_dir($cur_dir)) {
+            print($cur_dir);
+            mkdir($cur_dir);
+        }
+        $cur_dir .= "/".date("d");
+        if(!is_dir($cur_dir)) {
+            print($cur_dir);
+            mkdir($cur_dir);
+        }
+
+        $filename = $cur_dir.'/'.$this->filename;
+        return $filename;
+    }
+
+    public function writeData($filename) {
+        global $config;
+
+        $this->getComments();
+        print_r($this->comments);
+
+        $conn = new PDO("mysql:host=" . $config['mysql']['host'].";dbname=" . $config['mysql']['database'], $config['mysql']['user'], $config['mysql']['password']);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $data = $conn->query("SELECT * FROM sharif_items ORDER BY id")->fetchAll();
+
+        $csv_balance_header = "ID;Номенклатура;Количество;Стоимость;Комментарий\n";
+        $csv_balance_header = iconv("UTF-8", "windows-1251//IGNORE", $csv_balance_header);
+
+        file_put_contents($filename, $csv_balance_header);
+
+        foreach ($data as $row) {
+            $row_content = $row['id'].';'.$row['name'].';'.$row['amount'].';'.$row['price'];
+            if(array_key_exists($row['id'], $this->comments)) {
+                foreach($this->comments[$row['id']] as $comment) {
+                    $row_content .= ';'.$comment;
+                }
+            }
+            $row_content .= "\n";
+            
+            $row_content = iconv("UTF-8", "windows-1251//IGNORE", $row_content);
+            file_put_contents($filename, $row_content, FILE_APPEND);
+        }
+    }
+
+    public function getComments() {
+        global $config;
+        $this->comments = array();
+        foreach(range(2021, date("Y")) as $year) {
+            foreach(getMonths() as $month) {
+                foreach(range(1,31) as $day) {
+                    if($day < 10) {
+                        $day = '0'.$day;
+                    }
+                    $cur_dir = $config['data_directory']."/".$year.'/'.$month.'/'.$day;
+                    if(is_dir($cur_dir)) {
+                        foreach(scandir($cur_dir) as $filename) {
+                            if(strpos($filename, "user_reservations") === 0) {
+                                $row = 1;
+                                if (($handle = fopen($cur_dir.'/'.$filename, "r")) !== false) {
+                                    while (($data = fgetcsv($handle, 0, ";")) !== false) {
+                                        if($row > 1) {
+                                            $arr_data = $data[4].' - '.$data[5];
+                                            if(array_key_exists($data[1], $this->comments)) {
+                                                array_push($this->comments[$data[1]], $arr_data);
+                                            } else {
+                                                $this->comments[$data[1]] = array($arr_data);
+                                            }
+                                        }
+                                        $row++;
+                                    }
+                                    fclose($handle);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+?>
